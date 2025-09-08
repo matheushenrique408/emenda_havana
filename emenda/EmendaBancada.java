7ec852990450a0b84c71cc6ae1124c2dd04b64c2
matheushenrique408 <mham4@discente.ifpe.edu.br>
2025-09-08 01:03:53 -0300

    Update EmendaBancada.java

--- emenda/EmendaBancada.java
+++ emenda/EmendaBancada.java
@@ -1,18 +1,36 @@
 class EmendaBancada extends Emenda {
-    private java.util.List<String> membros = new java.util.ArrayList<String>();
+    private String membro1;
+    private String membro2;
+    private String membro3;
     private String linkAta;
 
     public EmendaBancada(String codigo, String autor, String uf, int ano, String linkAta) {
         super(codigo, autor, "Bancada", uf, ano);
         this.linkAta = linkAta;
+        this.membro1 = null;
+        this.membro2 = null;
+        this.membro3 = null;
     }
 
     public void adicionarMembro(String nome) {
-        membros.add(nome);
+        if (membro1 == null) {
+            membro1 = nome;
+        } else if (membro2 == null) {
+            membro2 = nome;
+        } else if (membro3 == null) {
+            membro3 = nome;
+        }
+      
     }
 
     public String paraTexto() {
-        return super.paraTexto() + " [Membros: " + membros.size() + "]";
+        int total = 0;
+        if (membro1 != null) total++;
+        if (membro2 != null) total++;
+        if (membro3 != null) total++;
+        return super.paraTexto() + " [Membros: " + total + "]";
     }
 }
+
+
 
